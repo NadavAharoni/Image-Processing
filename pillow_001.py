b4d4b7b5c1326f0e5b92ae88fdd60a5d618d3fcb
Nadav Aharoni <65665296+NadavAharoni@users.noreply.github.com>
2025-12-23 21:33:34 +0200

    Added code to display the red channel

--- pillow_001.py
+++ pillow_001.py
@@ -9,18 +9,32 @@ def main():
 
     file_name = sys.argv[1]
     img = PIL.Image.open(file_name)
-    # img.show()
+    img.show("original image")
 
     size = img.size
     print(size)
+    
+    # get pixel values through the getpixel function
     pixel = img.getpixel((0,0))
     print(pixel)
 
+    middle = size[0]//2, size[1]//2
+    print( img.getpixel(middle) )
+
+    # get pixel values through a PixelAccess object
+    # img.load() returns a PixelAccess object
     px = img.load()
-
+    print(middle)
     if px is not None:
-        print(px[size[0]//2,size[1]//2])
+        print( px[0,0] )
+        print( px[middle])
 
+    # display the red channel
+    r, g, b = img.split()
+    blank = PIL.Image.new('L', size, 0)
+    img_red = PIL.Image.merge("RGB", (r, blank, blank))
+    print(img_red.getpixel((0,0)))
+    img_red.show("red")
 
 if __name__ == "__main__":
     main()

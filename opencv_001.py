824ccb118834500f06067a5b968dfb7a23f94673
Nadav Aharoni <65665296+NadavAharoni@users.noreply.github.com>
2025-12-29 21:00:55 +0200

    Moved code into a "main" function
    Also added some printouts

--- opencv_001.py
+++ opencv_001.py
@@ -1,20 +1,24 @@
 import sys
 import cv2
 
-if len(sys.argv) < 2:
-    print(f"Usage: python {sys.argv[0]} image-filename")
-    exit(-1)
+def main():
+    if len(sys.argv) < 2:
+        print(f"Usage: python {sys.argv[0]} image-filename")
+        exit(-1)
 
-file_name = sys.argv[1]
-img = cv2.imread(file_name, cv2.IMREAD_COLOR)
-if img is None:
-    print(f"failed to read image from {file_name}")
-    exit(-2)
-          
-print((type(img)))
-# print(img)
+    file_name = sys.argv[1]
+    img = cv2.imread(file_name, cv2.IMREAD_COLOR)
+    if img is None:
+        print(f"failed to read image from {file_name}")
+        exit(-2)
+            
+    cv2.imshow(f"{file_name}", img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
-cv2.imshow(f"{file_name}", img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    print((type(img)))
+    print(f"img.ndim={img.ndim}, img.shape={img.shape}")
+    # print(img)
 
+if __name__ == "__main__":
+    main()

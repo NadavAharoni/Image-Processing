edff4a7c16bba5b32e8637a360dea2ce48ff6db6
NadavAharoni <nadav.aharoni@gmail.com>
2026-01-05 22:32:06 +0200

    Add tests for contrast

--- test_image_enhancements.py
+++ test_image_enhancements.py
@@ -1,4 +1,6 @@
 import matplotlib.pyplot as plt
+import numpy as np
+import cv2
 
 import create_test_images
 import brightten
@@ -6,7 +8,9 @@ import brightten
 def main():
     height = 300
     width = 400
-    fig, axes = plt.subplots(2, 2)
+
+    # create a "plot" with two rows and 4 columns
+    fig, axes = plt.subplots(2, 4, figsize=(10, 4))
 
     gradient_image = create_test_images.create_gradient_image(height, width)
     axes[0,0].imshow(gradient_image, cmap='gray')
@@ -24,6 +28,23 @@ def main():
     axes[0,1].axis('off')
 
     axes[1,1].axis('off')
+
+    # low_contrast_image = create_test_images.create_circle_image(height, width, 32, 35)
+    low_contrast_image = create_test_images.create_hidden_text_image(height, width)
+    axes[0,2].imshow(low_contrast_image, cmap='gray', vmin=0, vmax=255)
+    axes[0,2].set_title('low contrast')
+    axes[0,2].axis('off')
+
+    min_val = np.min(low_contrast_image)
+    max_val = np.max(low_contrast_image)
+    print(min_val, max_val)
+    image_normalized = (low_contrast_image - min_val) * ( 255 / (max_val - min_val) )
+    axes[1,2].imshow(image_normalized, cmap='gray', vmin=0, vmax=255)
+    axes[1,2].set_title('normalized')
+    axes[1,2].axis('off')
+
+    axes[1,3].axis('off')
+    axes[0,3].axis('off')
 
     plt.tight_layout()
     plt.show()

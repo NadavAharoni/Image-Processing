aad143685e67017aa16f9a252a46f5f6aabce546
Nadav Aharoni <65665296+NadavAharoni@users.noreply.github.com>
2026-01-20 00:41:57 +0200

    Made color offsets more dramatic
    Did this by changing the colors of the two
    existing rectangles, and adding another one.

--- compare_normalize.py
+++ compare_normalize.py
@@ -94,7 +94,7 @@ def equalize_ycrcb_brightness(img):
 def main():
     # Create test image
     height, width = 300, 400
-    bg_color = (120, 20, 20)
+    bg_color = (120, 30, 25)
     
     # Calculate 1/8 of image area (approximately)
     rect_height = height // 4
@@ -102,8 +102,9 @@ def main():
     
     # Two non-overlapping rectangles, each covering about 1/8 of image
     rect_list = [
-        {'rect': ((50, 50), (50 + rect_height, 50 + rect_width)), 'color': (140, 21, 21)},
-        {'rect': ((150, 250), (150 + rect_height, 250 + rect_width)), 'color': (150, 22, 22)}
+        {'rect': ((50, 50), (50 + rect_height, 50 + rect_width)), 'color': (135, 31, 22)},
+        {'rect': ((150, 250), (150 + rect_height, 250 + rect_width)), 'color': (150, 20, 30)},
+        {'rect': ((110, 160), (110 + 30, 160 + 40)), 'color': (100, 10, 29)}
     ]
     
     img = create_rects(height, width, 'RGB', bg_color, rect_list)
